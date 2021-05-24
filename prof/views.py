a35993b1fee886eba76209f0799ff7ee102befdb
louisenje <louisenje@gmail.com>
2021-05-24 11:43:07 +0300

    created model Image  and assigned attributes

--- prof/views.py
+++ prof/views.py
@@ -20,3 +20,30 @@ class Profile(models.Model):
     @classmethod
     def update(cls,user_id):
         pass
+    
+class Image(models.Model):
+    image=models.ImageField(upload_to='images/')
+    image_name=models.CharField(max_length=50)
+    image_caption=HTMLField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    profile=models.ForeignKey(Profile)
+    pub_time=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image_name
+    class Meta:
+        ordering = ['-pub_time']
+
+    @classmethod
+    def get_all(cls):
+        images=cls.objects.all()
+        return images
+
+    @classmethod
+    def get_specific(cls,user_id):
+        image=cls.objects.filter(user=user_id)
+        return image
+    @classmethod
+    def get_followed_image(cls,followers_array):
+        images=cls.objects.filter(user__in=followers_array)
+        return images
